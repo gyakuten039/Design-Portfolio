bce033046fcb74988fa761ba0cade8682945df89
gyakuten039 <2111304327@qq.com>
2026-09-10 11:10:16 +0800

    Create typing_game_en_US.ts

---
+++ 拯救苹果与太空大战打字小游戏/Coding/mui/typing_game_en_US.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>TypingHero</source>
+        <translation>TypingHero</translation>
+    </message>
+    <message>
+        <source>🏠 Home</source>
+        <translation>🏠 Home</translation>
+    </message>
+    <message>
+        <source>Welcome to TypingHero!</source>
+        <translation>Welcome to TypingHero!</translation>
+    </message>
+    <message>
+        <source>Choose a game to start your typing adventure</source>
+        <translation>Choose a game to start your typing adventure</translation>
+    </message>
+    <message>
+        <source>Select Game</source>
+        <translation>Select Game</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>Save Apple</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>Space Battle</translation>
+    </message>
+    <message>
+        <source>Typing Practice</source>
+        <translation>Typing Practice</translation>
+    </message>
+    <message>
+        <source>Speed Challenge</source>
+        <translation>Speed Challenge</translation>
+    </message>
+    <message>
+        <source>Word Master</source>
+        <translation>Word Master</translation>
+    </message>
+    <message>
+        <source>Brain Training</source>
+        <translation>Brain Training</translation>
+    </message>
+</context>
+<context>
+    <name>AppleGameWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Back</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>Back to Menu</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>Save Apple</translation>
+    </message>
+    <message>
+        <source>Type the words on the falling apples!</source>
+        <translation>Type the words on the falling apples!</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>Game Over</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>Final Score: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>Accuracy: %1%</translation>
+    </message>
+    <message>
+        <source>Score:</source>
+        <translation>Score:</translation>
+    </message>
+    <message>
+        <source>Missed:</source>
+        <translation>Missed:</translation>
+    </message>
+    <message>
+        <source>Combo:</source>
+        <translation>Combo:</translation>
+    </message>
+    <message>
+        <source>Accuracy:</source>
+        <translation>Accuracy:</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>Time:</translation>
+    </message>
+</context>
+<context>
+    <name>SpaceBattleWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Resume</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Back</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>Back to Menu</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>Space Battle</translation>
+    </message>
+    <message>
+        <source>Type the words to destroy enemy spaceships!</source>
+        <translation>Type the words to destroy enemy spaceships!</translation>
+    </message>
+    <message>
+        <source>Mission Complete</source>
+        <translation>Mission Complete</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>Final Score: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>Accuracy: %1%</translation>
+    </message>
+    <message>
+        <source>Enemies Destroyed: %1</source>
+        <translation>Enemies Destroyed: %1</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>Game Settings</translation>
+    </message>
+    <message>
+        <source>Fall Speed:</source>
+        <translation>Fall Speed:</translation>
+    </message>
+    <message>
+        <source>Spawn Count:</source>
+        <translation>Spawn Count:</translation>
+    </message>
+    <message>
+        <source>Spawn Interval (ms):</source>
+        <translation>Spawn Interval (ms):</translation>
+    </message>
+    <message>
+        <source>Audio Settings</source>
+        <translation>Audio Settings</translation>
+    </message>
+    <message>
+        <source>Music Volume:</source>
+        <translation>Music Volume:</translation>
+    </message>
+    <message>
+        <source>Quit Game</source>
+        <translation>Quit Game</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+</context>
+<context>
+    <name>AppleGameWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Resume</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Back</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>Back to Menu</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>Save Apple</translation>
+    </message>
+    <message>
+        <source>Type the words on the falling apples!</source>
+        <translation>Type the words on the falling apples!</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>Game Over</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>Final Score: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>Accuracy: %1%</translation>
+    </message>
+    <message>
+        <source>Score:</source>
+        <translation>Score:</translation>
+    </message>
+    <message>
+        <source>Missed:</source>
+        <translation>Missed:</translation>
+    </message>
+    <message>
+        <source>Combo:</source>
+        <translation>Combo:</translation>
+    </message>
+    <message>
+        <source>Accuracy:</source>
+        <translation>Accuracy:</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>Time:</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>Game Settings</translation>
+    </message>
+    <message>
+        <source>Fall Speed:</source>
+        <translation>Fall Speed:</translation>
+    </message>
+    <message>
+        <source>Spawn Count:</source>
+        <translation>Spawn Count:</translation>
+    </message>
+    <message>
+        <source>Spawn Interval (ms):</source>
+        <translation>Spawn Interval (ms):</translation>
+    </message>
+    <message>
+        <source>Audio Settings</source>
+        <translation>Audio Settings</translation>
+    </message>
+    <message>
+        <source>Music Volume:</source>
+        <translation>Music Volume:</translation>
+    </message>
+    <message>
+        <source>Quit Game</source>
+        <translation>Quit Game</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>TypingHero</source>
+        <translation>TypingHero</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <source>Welcome to TypingHero!</source>
+        <translation>Welcome to TypingHero!</translation>
+    </message>
+    <message>
+        <source>Choose a game to start your typing adventure</source>
+        <translation>Choose a game to start your typing adventure</translation>
+    </message>
+    <message>
+        <source>Select Game</source>
+        <translation>Select Game</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>Save Apple</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>Space Battle</translation>
+    </message>
+    <message>
+        <source>Typing Practice</source>
+        <translation>Typing Practice</translation>
+    </message>
+    <message>
+        <source>Speed Challenge</source>
+        <translation>Speed Challenge</translation>
+    </message>
+    <message>
+        <source>Word Master</source>
+        <translation>Word Master</translation>
+    </message>
+    <message>
+        <source>Brain Training</source>
+        <translation>Brain Training</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>Game Settings</translation>
+    </message>
+    <message>
+        <source>Game Speed:</source>
+        <translation>Game Speed:</translation>
+    </message>
+    <message>
+        <source>Enable Sound</source>
+        <translation>Enable Sound</translation>
+    </message>
+    <message>
+        <source>Enable AI Word Generation</source>
+        <translation>Enable AI Word Generation</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Appearance</translation>
+    </message>
+    <message>
+        <source>Dark Mode</source>
+        <translation>Dark Mode</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <source>Select Language</source>
+        <translation>Select Language</translation>
+    </message>
+    <message>
+        <source>Choose Language:</source>
+        <translation>Choose Language:</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+</context>
+</TS>
