65db4e0a21f0d98c84b90390bf8f828e8da4d5d5
gyakuten039 <2111304327@qq.com>
2026-09-10 11:11:39 +0800

    Create typing_game_zh_CN.ts

---
+++ 拯救苹果与太空大战打字小游戏/Coding/mui/typing_game_zh_CN.ts
@@ -0,0 +1,415 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>TypingHero</source>
+        <translation>打字英雄</translation>
+    </message>
+    <message>
+        <source>🏠 Home</source>
+        <translation>🏠 首页</translation>
+    </message>
+    <message>
+        <source>Welcome to TypingHero!</source>
+        <translation>欢迎来到打字英雄！</translation>
+    </message>
+    <message>
+        <source>Choose a game to start your typing adventure</source>
+        <translation>选择一个游戏开始您的打字冒险</translation>
+    </message>
+    <message>
+        <source>Select Game</source>
+        <translation>选择游戏</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>拯救苹果</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>太空大战</translation>
+    </message>
+    <message>
+        <source>Typing Practice</source>
+        <translation>打字练习</translation>
+    </message>
+    <message>
+        <source>Speed Challenge</source>
+        <translation>速度挑战</translation>
+    </message>
+    <message>
+        <source>Word Master</source>
+        <translation>单词大师</translation>
+    </message>
+    <message>
+        <source>Brain Training</source>
+        <translation>脑力训练</translation>
+    </message>
+</context>
+<context>
+    <name>AppleGameWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>重新开始</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>返回菜单</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>拯救苹果</translation>
+    </message>
+    <message>
+        <source>Type the words on the falling apples!</source>
+        <translation>输入下落苹果上的单词！</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>游戏结束</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>最终得分: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>准确率: %1%</translation>
+    </message>
+    <message>
+        <source>Score:</source>
+        <translation>得分:</translation>
+    </message>
+    <message>
+        <source>Missed:</source>
+        <translation>错过:</translation>
+    </message>
+    <message>
+        <source>Combo:</source>
+        <translation>连击:</translation>
+    </message>
+    <message>
+        <source>Accuracy:</source>
+        <translation>准确率:</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>时间:</translation>
+    </message>
+</context>
+<context>
+    <name>SpaceBattleWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>重新开始</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>返回菜单</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>太空大战</translation>
+    </message>
+    <message>
+        <source>Type the words to destroy enemy spaceships!</source>
+        <translation>输入单词摧毁敌方飞船！</translation>
+    </message>
+    <message>
+        <source>Mission Complete</source>
+        <translation>任务完成</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>最终得分: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>准确率: %1%</translation>
+    </message>
+    <message>
+        <source>Enemies Destroyed: %1</source>
+        <translation>摧毁敌人: %1</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>游戏设置</translation>
+    </message>
+    <message>
+        <source>Fall Speed:</source>
+        <translation>下落速度：</translation>
+    </message>
+    <message>
+        <source>Ship Speed:</source>
+        <translation>飞机速度：</translation>
+    </message>
+    <message>
+        <source>Spawn Count:</source>
+        <translation>生成数量：</translation>
+    </message>
+    <message>
+        <source>Spawn Interval (ms):</source>
+        <translation>生成间隔（毫秒）：</translation>
+    </message>
+    <message>
+        <source>Audio Settings</source>
+        <translation>音频设置</translation>
+    </message>
+    <message>
+        <source>Music Volume:</source>
+        <translation>音乐音量：</translation>
+    </message>
+    <message>
+        <source>Quit Game</source>
+        <translation>退出游戏</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>AppleGameWidget</name>
+    <message>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>重新开始</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+    <message>
+        <source>Back to Menu</source>
+        <translation>返回菜单</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>拯救苹果</translation>
+    </message>
+    <message>
+        <source>Type the words on the falling apples!</source>
+        <translation>输入下落苹果上的单词！</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>游戏结束</translation>
+    </message>
+    <message>
+        <source>Final Score: %1</source>
+        <translation>最终得分: %1</translation>
+    </message>
+    <message>
+        <source>Accuracy: %1%</source>
+        <translation>准确率: %1%</translation>
+    </message>
+    <message>
+        <source>Score:</source>
+        <translation>得分:</translation>
+    </message>
+    <message>
+        <source>Missed:</source>
+        <translation>错过:</translation>
+    </message>
+    <message>
+        <source>Combo:</source>
+        <translation>连击:</translation>
+    </message>
+    <message>
+        <source>Accuracy:</source>
+        <translation>准确率:</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>时间:</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>游戏设置</translation>
+    </message>
+    <message>
+        <source>Fall Speed:</source>
+        <translation>下落速度：</translation>
+    </message>
+    <message>
+        <source>Ship Speed:</source>
+        <translation>飞机速度：</translation>
+    </message>
+    <message>
+        <source>Spawn Count:</source>
+        <translation>生成数量：</translation>
+    </message>
+    <message>
+        <source>Spawn Interval (ms):</source>
+        <translation>生成间隔（毫秒）：</translation>
+    </message>
+    <message>
+        <source>Audio Settings</source>
+        <translation>音频设置</translation>
+    </message>
+    <message>
+        <source>Music Volume:</source>
+        <translation>音乐音量：</translation>
+    </message>
+    <message>
+        <source>Quit Game</source>
+        <translation>退出游戏</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>TypingHero</source>
+        <translation>打字英雄</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>首页</translation>
+    </message>
+    <message>
+        <source>Welcome to TypingHero!</source>
+        <translation>欢迎来到打字英雄！</translation>
+    </message>
+    <message>
+        <source>Choose a game to start your typing adventure</source>
+        <translation>选择一个游戏开始您的打字冒险</translation>
+    </message>
+    <message>
+        <source>Select Game</source>
+        <translation>选择游戏</translation>
+    </message>
+    <message>
+        <source>Save Apple</source>
+        <translation>拯救苹果</translation>
+    </message>
+    <message>
+        <source>Space Battle</source>
+        <translation>太空大战</translation>
+    </message>
+    <message>
+        <source>Typing Practice</source>
+        <translation>打字练习</translation>
+    </message>
+    <message>
+        <source>Speed Challenge</source>
+        <translation>速度挑战</translation>
+    </message>
+    <message>
+        <source>Word Master</source>
+        <translation>单词大师</translation>
+    </message>
+    <message>
+        <source>Brain Training</source>
+        <translation>脑力训练</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Game Settings</source>
+        <translation>游戏设置</translation>
+    </message>
+    <message>
+        <source>Game Speed:</source>
+        <translation>游戏速度：</translation>
+    </message>
+    <message>
+        <source>Enable Sound</source>
+        <translation>启用声音</translation>
+    </message>
+    <message>
+        <source>Enable AI Word Generation</source>
+        <translation>启用AI单词生成</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <source>Dark Mode</source>
+        <translation>深色模式</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Select Language</source>
+        <translation>选择语言</translation>
+    </message>
+    <message>
+        <source>Choose Language:</source>
+        <translation>选择语言：</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>英文</translation>
+    </message>
+</context>
+</TS>
